ca9fc29eb080b3fe35ea38f092ee1dc4d58708e8
hellfreezes <hellfreezes@yandex.ru>
2018-06-25 13:59:57 +0700

    Simple classes
    World
    Tile
    WorldController

---
+++ RimCraft/Assets/Controllers/WorldController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+    World world;
+
+	// Use this for initialization
+	void Start () {
+        world = new World();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
