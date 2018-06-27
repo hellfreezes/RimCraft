b3192e0b12a65f4f5f933ac170c7fbfa096b7a55
hellfreezes <hellfreezes@yandex.ru>
2018-06-27 12:04:32 +0700

    UI Starts

--- RimCraft/Assets/Controllers/MouseController.cs
+++ RimCraft/Assets/Controllers/MouseController.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour {
     [SerializeField]
-    GameObject circleCursorPrefab;      // Объект курсор
+    GameObject circleCursorPrefab;              // Объект курсор
 
 
-    Vector3 lastFramePosition;          // Позиция мыши взятая из предыдущего кадра
-    Vector3 dragStartPosition;          // Позиция мыши с которой начато перетаскивание
-    Vector3 currFramePosition;          // Позиция мыши в данный момент
+    Vector3 lastFramePosition;                  // Позиция мыши взятая из предыдущего кадра
+    Vector3 currFramePosition;                  // Позиция мыши в данный момент
 
-	// Use this for initialization
-	void Start () {
-		
+    Vector3 dragStartPosition;                  // Позиция мыши с которой начато перетаскивание
+    List<GameObject> dragPreviewGameObjects;    // Лист хранящий в себе маркеры выделенных тайлов
+
+    // Use this for initialization
+    void Start () {
+        dragPreviewGameObjects = new List<GameObject>();
 	}
 	
 	// Update is called once per frame
@@ -50,30 +52,8 @@ public class MouseController : MonoBehaviour {
     // Обработка нажатия левой кнопки мыши
     private void UpdateDragging()
     {
-        int start_x = Mathf.FloorToInt(dragStartPosition.x);
-        int end_x = Mathf.FloorToInt(currFramePosition.x);
-
-        // Если вдруг конечный x меньше начального, то меняем их значения местами 
-        if (end_x < start_x)
-        {
-            int tmp = end_x;
-            end_x = start_x;
-            start_x = tmp;
-        }
-
-        int start_y = Mathf.FloorToInt(dragStartPosition.y);
-        int end_y = Mathf.FloorToInt(currFramePosition.y);
-
-        // Если вдруг конечный y меньше начального, то меняем их значения местами 
-        if (end_y < start_y)
-        {
-            int tmp = end_y;
-            end_y = start_y;
-            start_y = tmp;
-        }
-
         // Это обработка drag&drop. Обработка прямоугольной области содержащей тайлы
-        // Начало перетаскивания
+        // Начало перетаскивания - фиксируем координаты тут
         if (Input.GetMouseButtonDown(0)) // нажата в предыдущем фрейме
         {
             //Запоминаем позицию с которой начали перетаскивание
@@ -81,7 +61,39 @@ public class MouseController : MonoBehaviour {
 
         }
 
-        if (Input.GetMouseButton(0)) // всё еще нажата
+        // Координаты начала перетаскивания и конца перетаскивания (из них строится прямоугольная область)
+        int start_x = Mathf.FloorToInt(dragStartPosition.x);
+        int end_x = Mathf.FloorToInt(currFramePosition.x);
+        int start_y = Mathf.FloorToInt(dragStartPosition.y);
+        int end_y = Mathf.FloorToInt(currFramePosition.y);
+
+        // Если конечный x меньше начального, то меняем их значения местами 
+        if (end_x < start_x)
+        {
+            int tmp = end_x;
+            end_x = start_x;
+            start_x = tmp;
+        }
+        // Если конечный y меньше начального, то меняем их значения местами 
+        if (end_y < start_y)
+        {
+            int tmp = end_y;
+            end_y = start_y;
+            start_y = tmp;
+        }
+
+
+        // Подчищаем ненужные копии маркеров каждый фрейм
+        while(dragPreviewGameObjects.Count > 0)
+        {
+            // TODO: не самое лучше решение на мой взгляд
+            // Находит ВСЕ маркеры и деспавнит их КАЖДЫЙ фрейм
+            GameObject go = dragPreviewGameObjects[0];
+            dragPreviewGameObjects.RemoveAt(0);
+            SimplePool.Despawn(go);
+        }
+
+        if (Input.GetMouseButton(0)) // всё еще нажата - это процесс перетаскивания с зажатой лв. клавишей мыши
         {
             // Отобразить сетку подсказок, какие тайлы попали в область выделения
             // Перебираем все тайлы попавшие в прямоугольную область описанную перетаскиванием
@@ -90,17 +102,20 @@ public class MouseController : MonoBehaviour {
             {
                 for (int y = start_y; y <= end_y; y++)
                 {
-                    // Отображаем подсказку на каждом тайле
+                    // Отображаем подсказку на каждом тайле - это маркер в данный момент в виде кружка
+                    // Визуализация маркера происходит с помощью отображения копии circleCursorPrefab
                     Tile t = WorldController.Instance.World.GetTileAt(x, y);
                     if (t != null)
                     {
-                        Instantiate(circleCursorPrefab, new Vector3(x, y, 0), Quaternion.identity);
+                        GameObject go = SimplePool.Spawn(circleCursorPrefab, new Vector3(x, y, 0), Quaternion.identity);
+                        go.transform.SetParent(this.transform, true);
+                        dragPreviewGameObjects.Add(go);
                     }
                 }
             }
         }
 
-        // Конец перетаскивания
+        // Конец перетаскивания - фиксируем тут координаты
         if (Input.GetMouseButtonUp(0)) // отпущена
         {
             // Перебираем все тайлы попавшие в прямоугольную область описанную перетаскиванием
@@ -129,6 +144,9 @@ public class MouseController : MonoBehaviour {
 
             Camera.main.transform.Translate(diff);
         }
+
+        Camera.main.orthographicSize -= Camera.main.orthographicSize * Input.GetAxis("Mouse ScrollWheel");
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 3f, 25f);
     }
 
     public Tile GetTileAtWorldCoord(Vector3 coord)
